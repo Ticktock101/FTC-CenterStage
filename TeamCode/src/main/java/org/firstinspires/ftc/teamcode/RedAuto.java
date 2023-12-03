6283da851885e864e76de1929e88a160677e4041
Tacocat0254 <tacocat0254@gmail.com>
2023-12-03 11:21:53 -0600

    auto branch

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAuto.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.apache.commons.math3.geometry.euclidean.twod.Line;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+
+@Autonomous (name = "RedAuto")
+public class RedAuto extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
