d02a802b4ea92f866f90aa3e026655a20b23ba09
Tacocat0254 <tacocat0254@gmail.com>
2024-01-13 12:18:12 -0600

    Update BlueAuto.java
    auto works!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueAuto.java
@@ -31,6 +31,7 @@ package org.firstinspires.ftc.teamcode;
 
 //package org.firstinspires.ftc.robotcontroller.external.samples;
 
+import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -42,6 +43,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
@@ -82,10 +84,8 @@ public class BlueAuto extends LinearOpMode {
     private double fast = 0.5; // Limit motor power to this value for Andymark RUN_USING_ENCODER mode
     private double medium = 0.3; // medium speed
     private double slow = 0.1; // slow speed
-    private double clicksPerInch = 50.238; // empirically measured
-    private double clicksPerDeg = 13.55; // empirically measured
-    private double lineThreshold = 0.7; // floor should be below this value, line above
-    private double redThreshold = 1.9; // red should be below this value, blue above
+    private double clicksPerInch = 62.7863478; // empirically measured
+    private double clicksPerDeg = 1.5555555555556; // empirically measured
 
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
 
@@ -105,13 +105,16 @@ public class BlueAuto extends LinearOpMode {
      */
     private TfodProcessor tfod;
 
-    private int position = 2;
+    private int position = 1;
     private int count = 0;
 
     private boolean isFound = false;
 
     private int countFalse = 0;
     private int laPos; private int raPos;
+
+    private RevColorSensorV3 rightColor;
+    RevColorSensorV3 leftColor;
 
 
     /**
@@ -130,25 +133,23 @@ public class BlueAuto extends LinearOpMode {
 
 
 
-//        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
-//        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
-//
-//        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
 
-//        setZeroPosition();
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-
+        setZeroPosition();
 
         rightArm1 = hardwareMap.get(DcMotor.class, "rightArm");
         leftArm1 = hardwareMap.get(DcMotor.class, "leftArm");
-//        rightArm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        leftArm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightArm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftArm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-//        rightArm1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-//        leftArm1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightArm1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftArm1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 
         rightClaw = hardwareMap.get(Servo.class, "rightServo");
@@ -158,6 +159,9 @@ public class BlueAuto extends LinearOpMode {
         pixelPush = hardwareMap.get(Servo.class, "pixel");
         pixelPush.setDirection(Servo.Direction.REVERSE);
 
+        rightColor = hardwareMap.get(RevColorSensorV3.class, "rightColor");
+        leftColor = hardwareMap.get(RevColorSensorV3.class, "leftColor");
+
         rightFront.setDirection(DcMotor.Direction.FORWARD);
         leftFront.setDirection(DcMotor.Direction.REVERSE);
         rightBack.setDirection(DcMotor.Direction.FORWARD);
@@ -166,23 +170,23 @@ public class BlueAuto extends LinearOpMode {
         rightArm1.setDirection(DcMotorSimple.Direction.REVERSE);
         leftArm1.setDirection(DcMotor.Direction.FORWARD);
 
-//        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        leftArm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        rightArm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftArm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightArm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-//        wristMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        wristMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Set the drive motor run modes:
         leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        leftArm1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        rightArm1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        wristMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftArm1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightArm1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        wristMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
 //        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 //        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -190,6 +194,12 @@ public class BlueAuto extends LinearOpMode {
 //        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 //        leftArm1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 //        rightArm1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // Wait for the DS start button to be touched.
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch Play to start OpMode");
+        telemetry.update();
+        waitForStart();
 
         initTfod();
 
@@ -206,99 +216,75 @@ public class BlueAuto extends LinearOpMode {
 //                telemetryTfod();
 
 //
-                ElapsedTime myLoopTimer = new ElapsedTime();
+
 
                 while (count == 0)
                 {
+                    moveForward(22, medium);
 
-                    while (myLoopTimer.time() < 8 && !telemetryTfod())
+                    if (leftColor.getDistance(DistanceUnit.CM) < rightColor.getDistance(DistanceUnit.CM))
                     {
-                        telemetryTfod();
+                        position = 0;
+                    }
+                    if (leftColor.getDistance(DistanceUnit.CM) > rightColor.getDistance(DistanceUnit.CM))
+                    {
+                        position = 2;
+                    }
+                    if (leftColor.getDistance(DistanceUnit.CM) > 4 && rightColor.getDistance(DistanceUnit.CM) > 4)
+                    {
+                        position = 1; 
                     }
 
-//                    rightClaw.setPosition(0);
-//                    leftClaw.setPosition(0);
-
-                    if (position == 0)
+                    if(position == 0)
                     {
-                        moveForward(8, medium);
-                        turnClockwise(-20, 0.5);
-                        moveForward(11, medium);
+                        position = 0;
 
-                        sleep(400);
+                        turnClockwise(-600, medium);
+                        ElapsedTime myLoopTimer = new ElapsedTime();
 
-
-
-                        ElapsedTime newTimer = new ElapsedTime();
-
-                        while (newTimer.time() < 3)
+                        while (myLoopTimer.time() < 3)
                         {
                             moveServo();
-
                         }
-//                        sleep(500);
 
-
-                        moveForward(-8, medium);
-                        turnClockwise(-40, 0.5);
-                        moveForward(35, medium);
-
-//                        moveArm(50, medium);
-//                        moveWrist(1);
-//
-//                        rightClaw.setPosition(0.1);
-//                        leftClaw.setPosition(0);
+                        moveForward(-2, medium);
                     }
 
-                    else if (position == 1)
+                    if(position == 2)
                     {
-                        moveForward(25, medium);
+                        position = 2;
 
-                        ElapsedTime newTimer = new ElapsedTime();
+                        turnClockwise(350, medium);
+                        ElapsedTime myLoopTimer = new ElapsedTime();
 
-                        while (newTimer.time() < 3)
+
+
+                        while (myLoopTimer.time() < 3)
                         {
                             moveServo();
-
                         }
 
-                        moveForward(-17, medium);
-
-                        turnClockwise(-35, medium);
-                        moveForward(35, medium);
-
-//                        moveArm(50, medium);
-//                        moveWrist(1);
-//
-//                        rightClaw.setPosition(0.1);
-//                        leftClaw.setPosition(0);
+                        moveForward(-5, medium);
                     }
-                    else
+
+                    if(position == 1)
                     {
-                        moveForward(13, medium);
-                        turnClockwise(13, 0.5);
-                        moveForward(7, medium);
+                        position = 1;
+                        ElapsedTime myLoopTimer = new ElapsedTime();
 
-                        ElapsedTime newTimer = new ElapsedTime();
+                        moveForward(1, medium);
 
-                        while (newTimer.time() < 3)
+//                        moveForward(2, medium);
+                        while (myLoopTimer.time() < 3)
                         {
                             moveServo();
-
                         }
 
-                        moveForward(-13, medium);
-
-                        turnClockwise(-54, medium);
-
-                        moveForward(35, medium);
-
-//                        moveArm(50, medium);
-//                        moveWrist(1);
-//
-//                        rightClaw.setPosition(0.1);
-//                        leftClaw.setPosition(0);
+                        moveForward(-2, medium);
                     }
+
+                    telemetry.addData("position", position);
+                    telemetry.update();
 
 
                     count++;
@@ -330,6 +316,7 @@ public class BlueAuto extends LinearOpMode {
         // Save more CPU resources when camera is no longer needed.
         visionPortal.close();
 
+
     }   // end runOpMode()
 
     public void moveServo()
@@ -347,22 +334,9 @@ public class BlueAuto extends LinearOpMode {
         // Create the TensorFlow processor by using a builder.
         tfod = new TfodProcessor.Builder()
 
-                // With the following lines commented out, the default TfodProcessor Builder
-                // will load the default model for the season. To define a custom model to load,
-                // choose one of the following:
-                //   Use setModelAssetName() if the custom TF Model is built in as an asset (AS only).
-                //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
                 .setModelAssetName(TFOD_MODEL_ASSET)
-                //.setModelFileName(TFOD_MODEL_FILE)
 
-                // The following default settings are available to un-comment and edit as needed to
-                // set parameters for custom models.
                 .setModelLabels(LABELS)
-                //.setIsModelTensorFlow2(true)
-                //.setIsModelQuantized(true)
-                //.setModelInputSize(300)
-                //.setModelAspectRatio(16.0 / 9.0)
-
                 .build();
 
         // Create the vision portal by using a builder.
@@ -375,32 +349,9 @@ public class BlueAuto extends LinearOpMode {
             builder.setCamera(BuiltinCameraDirection.BACK);
         }
 
-        // Choose a camera resolution. Not all cameras support all resolutions.
-        //builder.setCameraResolution(new Size(640, 480));
-
-        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
-        //builder.enableLiveView(true);
-
-        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
-        //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
-
-        // Choose whether or not LiveView stops if no processors are enabled.
-        // If set "true", monitor shows solid orange screen if no processors enabled.
-        // If set "false", monitor shows camera view without annotations.
-        //builder.setAutoStopLiveView(false);
-
-        // Set and enable the processor.
         builder.addProcessor(tfod);
 
-        // Build the Vision Portal, using the above settings.
         visionPortal = builder.build();
-
-        // Set confidence threshold for TFOD recognitions, at any time.
-        //tfod.setMinResultConfidence(0.75f);
-
-        // Disable or re-enable the TFOD processor at any time.
-        //visionPortal.setProcessorEnabled(tfod, true);
-
     }   // end method initTfod()
 
     /**
@@ -614,14 +565,10 @@ public class BlueAuto extends LinearOpMode {
         leftBack.setTargetPosition(lrPos);
         rightBack.setTargetPosition(rrPos);
 
-
-
         leftFront.setPower(speed);
         rightFront.setPower(speed);
         leftBack.setPower(speed);
         rightBack.setPower(speed);
-
-
 
         // wait for move to complete
         while (leftFront.isBusy() && rightFront.isBusy() &&
@@ -642,39 +589,6 @@ public class BlueAuto extends LinearOpMode {
         leftBack.setPower(0);
         rightBack.setPower(0);
     }
-    private void moveToLine(int howMuch, double speed) {
-        // howMuch is in inches. The robot will stop if the line is found before
-        // this distance is reached. A negative howMuch moves left, positive moves right.
-
-        // fetch motor positions
-        lfPos = leftFront.getCurrentPosition();
-        rfPos = rightFront.getCurrentPosition();
-        lrPos = leftBack.getCurrentPosition();
-        rrPos = rightBack.getCurrentPosition();
-
-        // calculate new targets
-        lfPos += howMuch * clicksPerInch;
-        rfPos -= howMuch * clicksPerInch;
-        lrPos -= howMuch * clicksPerInch;
-        rrPos += howMuch * clicksPerInch;
-
-        // move robot to new position
-        leftFront.setTargetPosition(lfPos);
-        rightFront.setTargetPosition(rfPos);
-        leftBack.setTargetPosition(lrPos);
-        rightBack.setTargetPosition(rrPos);
-        leftFront.setPower(speed);
-        rightFront.setPower(speed);
-        leftBack.setPower(speed);
-        rightBack.setPower(speed);
-
-        // Stop all motion;
-        leftFront.setPower(0);
-        rightFront.setPower(0);
-        leftBack.setPower(0);
-        rightBack.setPower(0);
-
-    }
 
     public void setZeroPosition()
     {
@@ -682,7 +596,6 @@ public class BlueAuto extends LinearOpMode {
         rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
     }
 
 }   // end class
